83c6e099d562b772e8b9033b8abe15fa4e4e3abb
Aliaksandr Matveichykau <kobuqa@gmail.com>
2023-03-15 20:17:24 +0300

    feat: factory example

---
+++ 0-creational/0-factory.ts
@@ -0,0 +1,50 @@
+/**
+ * Solves: Lowering high coupling.
+ * How: Creating instances by type
+ * Condition: Instances has to match the same interface
+ */
+
+type Platform = 'youtube' | 'twitch';
+
+type Config = {
+    channel: string;
+    muted: boolean;
+}
+
+type Player = {
+    url: string;
+}
+
+const createYoutubePlayer = (config: Config): Player => {
+    const player = {
+        url: `https://www.youtube.com/embed/${config.channel}?mute=${Number(config.muted)}`,
+        // ... additional properties, methods
+    }
+
+    return player;
+}
+
+const createTwitchPlayer = (config: Config): Player => {
+    const player = {
+        url: `https://player.twitch.tv/?channel=${config.channel}&muted=${config.muted}`,
+    }
+
+    return player;
+}
+
+
+const playerFactory = (platform: Platform) => {
+
+    const players: Record<Platform, (config: Config) => Player> = {
+        youtube: createYoutubePlayer,
+        twitch: createTwitchPlayer
+    }
+
+    return players[platform];
+}
+
+const youtubeCreator = playerFactory('youtube');
+const twitchCreator = playerFactory('twitch');
+
+const youtubePlayer = youtubeCreator({ channel: 'BBC News', muted: true })
+const twitchPlayer = twitchCreator({ channel: 'Asmongold', muted: false })
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # GoF 
 ## Here is my understanding of GoF patterns.
+### The main reason is to understand how to use it in FP-like approach
