fd7117c31ee92257ac74fe6260c3fa182529c410
Alexander Matveichikov <alexander.matveichikov@mbicycle.com>
2023-03-16 16:06:46 +0300

    feat: changed abstract factory example

--- 0-creational/1-abstract-factory.ts
+++ 0-creational/1-abstract-factory.ts
@@ -1,79 +1,50 @@
-/**
- * Solves: Creation of items famiilies  without specifying concrete item.  Lowering high coupling.
- * How: Abstract factory define all items producing by factory. 
- * Condition: Instances has to match the same interface
- */
+type Brand = "reebok" | "nike";
 
-type Material = 'wood' | 'metal';
+type Shoe = {
+  logo: Brand;
+  size: number;
+};
 
-interface Hammer {
-    material: Material;
-    slam(): void;
+type Shirt = {
+  logo: Brand;
+  size: "s" | "m" | "xl";
+};
+
+type WearFactory = {
+  makeShoe(size: number): Shoe;
+  makeShirt(size: "s" | "m" | "xl"): Shirt;
+};
+
+function createNikeFactory(): WearFactory {
+  return {
+    makeShoe: (size: number): Shoe => ({ logo: "nike", size }),
+    makeShirt: (size: "s" | "m" | "xl"): Shirt => ({ logo: "nike", size }),
+  };
 }
 
-interface Axe {
-    material: Material;
-    cut(): void;
+function createReebokFactory(): WearFactory {
+  return {
+    makeShoe: (size: number): Shoe => ({ logo: "reebok", size }),
+    makeShirt: (size: "s" | "m" | "xl"): Shirt => ({ logo: "reebok", size }),
+  };
 }
 
-interface InstrumentFactory {
-    createHammer(): Hammer;
-    createAxe(): Axe;
+function createWearFactory(brand: Brand): WearFactory {
+  const factories = {
+    nike: createNikeFactory,
+    reebok: createReebokFactory,
+  };
+
+  return factories[brand]();
 }
 
+const nikeFactory = createWearFactory("nike");
+const reebokFactory = createWearFactory("reebok");
 
-const createMetalFactory = (): InstrumentFactory => {
-    return {
-        createAxe() {
-            return {
-                material: 'metal',
-                cut: () =>  console.log('cutting with sharp edge!')
-            }
-        },
-        createHammer: () => {
-            return {
-                material: 'metal',
-                slam: () => console.log('slamming with metal sound!')
-            }
-        }
-    }
+// clientCode
+function makeShoe(factory: WearFactory, size: number): Shoe {
+  return factory.makeShoe(size);
 }
 
-const createWoodFactory = (): InstrumentFactory => {
-    return {
-        createAxe() {
-            return {
-                material: 'wood',
-                cut: () =>  console.log('cutting with blunt edge!')
-            }
-        },
-        createHammer: () => {
-            return {
-                material: 'wood',
-                slam: () => console.log('slamming with clunk sound!')
-            }
-        }
-    }
-}
-
-const factoryCreator = (type: Material): InstrumentFactory => {
-    const instrumentFactories = {
-        metal: createMetalFactory,
-        wood: createWoodFactory
-    }
-    return instrumentFactories[type]()
-}
-
-// Factories by Type
-const woodFactory = factoryCreator('wood');
-const metalFactory = factoryCreator('metal');
-
-// Client Code that uses abstract Factory as param
-const createAxe = (factory: InstrumentFactory): Axe => factory.createAxe()
-const createHammer = (factory: InstrumentFactory): Hammer => factory.createHammer()
-
-// Combinations of items provided by different factories
-const metalAxe = createAxe(metalFactory);
-const metalHammer = createHammer(metalFactory);
-const woodAxe = createAxe(woodFactory);
-const woodHammer = createHammer(woodFactory);
+const nikeShoe = makeShoe(nikeFactory, 41);
+const reebokShoe = makeShoe(reebokFactory, 42);
