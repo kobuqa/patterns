6e57f4650d773a253ef7a375fd6f9e51e5d92ed1
Alexander Matveichikov <alexander.matveichikov@mbicycle.com>
2023-03-16 17:38:52 +0300

    feat: lint to single style

--- 1-structural/0-adapter.ts
+++ 1-structural/0-adapter.ts
@@ -11,27 +11,21 @@ interface NewInterface {
 }
 
 const oldReader: OldInterface = {
-  get() {
-    console.log("get old");
-  },
+  get: () => console.log("get old"),
 };
 
 const newReader: NewInterface = {
-  obtain() {
-    console.log("get new");
-  },
+  obtain: () => console.log("get new"),
 };
 
 const oldToNewAdapter = (a: OldInterface): NewInterface => ({ obtain: a.get });
 
 // clientCode
 
-function getSmth(reader: NewInterface) {
-  reader.obtain();
-}
+const getData = (reader: NewInterface) => reader.obtain();
 
-getSmth(newReader); // works perfect since it's the same interfaces
+getData(newReader); // works perfect since it's the same interfaces
 
 // getSmth(oldReader); // error since incompatible interfaces
 
-getSmth(oldToNewAdapter(oldReader)); // works perfect since adapted
+getData(oldToNewAdapter(oldReader)); // works perfect since adapted
