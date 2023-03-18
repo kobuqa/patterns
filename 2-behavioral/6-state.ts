43b5e31e98f87281e35ecb9a8c2fdedb5009ea90
Aliaksandr Matveichykau <kobuqa@gmail.com>
2023-03-18 22:29:17 +0300

    fix: another state example

--- 2-behavioral/6-state.ts
+++ 2-behavioral/6-state.ts
@@ -1,46 +1,76 @@
 /**
  * Problem: Managing huge amount of states. Avoid 'Boolean Explosion'
  * Note: Cant be implemented separately from state machine. Ready solution xState. Even reducer - state machine :)
+ * BL: Payment flow, access via subscriptions, etc
  */
 
-type Action = string
-type NewState = string
+// ! Example is too simple to provide the profits of state machine. You will discover it on more complex nested states.
 
-interface State {
-    id: string;
-    on?: Record<Action, NewState>
-    states: Record<string, Required<Omit<State, 'states'>>>
+enum State {
+    RED,
+    RED_AND_YELLOW,
+    GREEN,
+    BLINKING_GREEN,
+    SHORT_YELLOW
 }
 
-const stateMachineConfig: State = {
-    id: 'stopLight',
-    states: {
-        red: {
-            id: 'red',
-            on: { YELLOW: 'yellow' }
+const stopLight  = {
+    state: State.RED,
+    do(){
+        this.config[this.state].do.bind(this)()
+    },
+    config: {
+        [State.RED]: {
+            do: function() {
+                console.log('[Stop Light]: Red. Wait.')
+                const id = setTimeout(() => {
+                    this.state = State.RED_AND_YELLOW
+                    this.do()
+                    clearTimeout(id)
+                 }, 3000)
+            } 
         },
-        yellow: {
-            id:  'yellow',
-            on: { GREEN: 'green' }
+        [State.RED_AND_YELLOW]: {
+                do: function() {
+            console.log('[Stop Light]: Red & Yellow. Be ready.')
+                const id = setTimeout(() => {
+                    this.state = State.GREEN
+                    this.do()
+                    clearTimeout(id)
+                 }, 2000)
+            } 
         },
-        green: {
-            id: 'green',
-            on: { RED: 'red' }
+        [State.GREEN]: {
+            do: function() {
+                console.log('[Stop Light]: Green. You can ride.')
+                const id = setTimeout(() => {
+                    this.state = State.BLINKING_GREEN
+                    this.do()
+                    clearTimeout(id)
+                 }, 5000)
+            }
+        },
+        [State.BLINKING_GREEN]: {
+            do: function() {
+                console.log('[Stop Light]: Blinking green. Finish maneuver.')
+                const id = setTimeout(() => {
+                    this.state = State.SHORT_YELLOW
+                    this.do()
+                    clearTimeout(id)
+                 }, 3000)
+            }
+        },
+        [State.SHORT_YELLOW]: {
+            do: function() {
+                console.log('[Stop Light]: Yellow. Stop if you can.')
+                const id = setTimeout(() => {
+                    this.state = State.RED
+                    this.do()
+                    clearTimeout(id)
+                 }, 1000)
+            }
         }
     }
 }
 
-const createFiniteMachine = (config: State) => {
-    return {
-        state: config.states.red,
-        transition: function (type) {
-            this.state = config.states[this.state.on[type]];
-        }
-    }
-}
-
-const fsm = createFiniteMachine(stateMachineConfig);
-
-fsm.transition('YELLOW');
-fsm.transition('GREEN');
-fsm.transition('RED');
+stopLight.do();
